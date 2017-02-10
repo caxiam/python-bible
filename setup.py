9b34142bad4af77185ac126de34601405636d3b4
Colton Allen <cmanallen90@gmail.com>
2017-02-10 15:56:02 -0500

    Added packages_dir setup command

--- setup.py
+++ setup.py
@@ -1,4 +1,9 @@
 from setuptools import setup
 
 
-setup(name='python-bible', version='0.1', py_modules=['bible'])
+setup(
+    name='python-bible',
+    version='0.1',
+    py_modules=['bible'],
+    package_dir={'bible': 'bible'},
+)
